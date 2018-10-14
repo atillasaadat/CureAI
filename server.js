dcfe4ca884142c31343f3b195c2c1529126c9fa5
Menushka Weeratunga <tmweeratunga@gmail.com>
2018-10-13 23:18:24 -0400

    Added user sign in and mongdb

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 'use strict';
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 //const assignments = require('./data.json');
+
+const uri = "mongodb+srv://test_user:c4cNxAy2cvMVd9RI@cureai-xicbd.gcp.mongodb.net/test?retryWrites=true";
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
 
 // App
 const app = express();
@@ -33,5 +35,95 @@ app.post('/clinc', (req, res) => {
     res.send(req.body);
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// Authenticate and Get User
+app.get('/users/auth/:user/:pass', function (req, res) {
+    console.log("gay");
+    var user = req.params.user;
+    var pass = req.params.pass;
+
+    MongoClient.connect(uri, function(err, client) {
+        const collection = client.db("CureAI").collection("users");
+        collection.findOne({ username: user, password: pass }).then((doc) => {
+
+            if (doc) {
+                res.status(200).json({
+                    status: "success",
+                    user: doc
+                })
+            } else {
+                res.status(200).json({
+                    status: "failed"
+                })
+            }
+
+            client.close();
+        });
+    });
+})
+
+// Create User
+app.get('/users/create/:user/:email/:pass/', function (req, res) {
+    var user = req.params.user;
+    var mail = req.params.email;
+    var pass = req.params.pass;
+
+    MongoClient.connect(uri, function(err, client) {
+        const collection = client.db("CureAI").collection("users");
+        const doc = {
+            username: user,
+            email: mail,
+            password: pass,
+            firstName: '',
+            lastName: '',
+            birthday: '',
+            gender: '',
+            occupation: '',
+            relationshipStatus: ''
+        };
+
+        collection.insertOne(doc).then((result) => {
+            console.log(result);
+            res.status(200).json({
+                status: "success",
+                user: doc
+            })
+            client.close();
+        });
+    });
+})
+
+// More Info
+app.get('/users/moreInfo/:user/:first/:last/:birth/:sex/:job/:relation/', function (req, res) {
+    var user = req.params.user;
+    var first = req.params.first;
+    var last = req.params.last;
+    var birth = req.params.birth;
+    var sex = req.params.sex;
+    var job = req.params.job;
+    var relation = req.params.relation;
+
+    MongoClient.connect(uri, function(err, client) {
+        const collection = client.db("CureAI").collection("users");
+        const changes = {
+            firstName: first,
+            lastName: last,
+            birthday: birth,
+            gender: sex,
+            occupation: job,
+            relationshipStatus: relation
+        };
+
+        collection.findOneAndUpdate({ username: user }, { $set: changes }, {returnNewDocument: true}).then((doc) => {
+            res.status(200).json({
+                status: "success",
+                user: doc
+            })
+            client.close();
+        });
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on port ` + PORT);
+});
+
